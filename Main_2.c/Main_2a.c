c62685a73caf595c71cd01c539b827271f535480
mehetajuddin <mehatajahmed001@gmail.com>
2024-12-20 20:19:54 +0600

    Mai

---
+++ Main_2.c/Main_2a.c
@@ -0,0 +1,14 @@
+/*2. Calculate the area of a circle.*/
+
+
+#include <stdio.h>
+
+  int main () {
+
+    float r = 9; // radius of the circle; r = radius;
+    float pi = 3.14; // value of pi;
+    
+    printf("The area of the circle with radius of %f is %f",r, pi*r*r);
+    
+      return 0;
+}
